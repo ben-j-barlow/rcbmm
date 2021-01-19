d796998a9f40b0980c256b8ccfd5ba21c99f7324
Ben Barlow <ben.j.barlow.1@gmail.com>
2021-01-19 19:14:47 +0000

    Bug fixes in CM2 (try-error related)

--- R/cm.step.2.R
+++ R/cm.step.2.R
@@ -29,92 +29,41 @@
 #' 
 #' @export
 #' 
-cm.step.2 <- function(x, K, z, mvdc, margins, lambda, trace = T) {
+cm.step.2 <- function(x, K, z, mvdc, margins, lambda, trace = TRUE) {
   p <- ncol(x)
-  
-  # converts angles to whole real line prior to optimization
-  trans.ang <- function(ang) {
-    tan((ang + pi)/2)
-  }
-  
-  # converts transformed angles back to their correct value during optimization
-  inversetrans.ang <- function(x) {
-    2*atan(x) + pi
-  }
-  
-  # computes cumulative probabilities for a given mixture component
-  compute.u <- function(x, margins, marginal_params) {
-    sapply(1:p, function(t) {
-      pars <-  as.numeric(marginal_params[[t]])
-      switch(margins[t],
-             norm = stats::pnorm(x[, t], pars[1], pars[2]),
-             gamma = stats::pgamma(x[, t], shape = pars[1], rate = pars[2]),
-             beta = stats::pbeta(x[, t], shape1 = pars[1], shape2 = pars[2])
-      )
-    })
-  }
-  
-  
-  CM.2.optimiser <- function(param, post_probs, copula, u, lambda) {
-    # prepare copula object using angles parameters parsed
-    param <- inversetrans.ang(param)
-    cop_param <- copula::P2p(angles2rho(p2P.angles(param)))
-    copula@parameters <- cop_param
-    
-    if (any(is.na(u)))
-      return(-Inf)
-    
-    inds1 <- post_probs > 1e-16
-    res <- post_probs * log(copula::dCopula(u, copula = copula))
-    
-    # compute unpenalized likelihood
-    comp_1 <- sum(res[inds1])
-    # compute shrinkage pentaly
-    comp_2 <- lambda * sum((param - (pi/2))^2)
-    
-    return(comp_1 - comp_2)
-  }
-  
-  
-  expr <- function(j, method) {
-    # compute cumulative probabilities for given mixture component
-    u <- compute.u(x = x, margins = margins, marginal_params = mvdc[[j]]@paramMargins)
-    u[u > 0.999] <- 0.999
-    u[u < 0.001] <- 0.001
-    
-    # prepare copula parameters for optimization 
-    start <- mvdc[[j]]@copula@parameters
-    start <- P2p.angles(rho2angles(copula::p2P(start)))
-    start <- trans.ang(start)
-    
-    # use optimizer to find likelihood maximum
-    optim_out <- stats::optim(par = start, 
-                              fn = CM.2.optimiser,
-                              method = method,
-                              control = list(fnscale = -1, trace = F),
-                              post_probs = z[, j], 
-                              u = u, 
-                              copula = mvdc[[j]]@copula,
-                              lambda = lambda)
-    
-    # collate and return shrinkage penalty and new value of copula parameters returned by optimizer
-    res <- optim_out$par
-    res <- inversetrans.ang(res)
-    penalty <- lambda * sum((res - (pi/2))^2)
-    
-    res <- list(param = copula::P2p(angles2rho(p2P.angles(res))), 
-                pen = penalty)
-  }
-  
   # perform for each mixture component in parallel
-  result <- lapply(1:K,
-                   function(j) parallel::mcparallel(expr(j, method = "BFGS"), name = j))
-  result <- parallel::mccollect(result)
+  #result <- lapply(1:K,
+  #                 function(j) parallel::mcparallel(cm.step.2.component(j = j, 
+  #                                                                      method = "BFGS", 
+  #                                                                      x = x, 
+  #                                                                      margins = margins, 
+  #                                                                      component = mvdc[[j]], 
+  #                                                                      z = z, 
+  #                                                                      lambda = lambda)
+  #                                                  , name = j))
+  #result <- parallel::mccollect(result)
+  result <- list()
+  for (j in 1:K) {
+    result[[j]] <- try(cm.step.2.component(j = j, 
+                       method = "BFGS", 
+                       x = x, 
+                       margins = margins,
+                       component = mvdc[[j]], 
+                       z = z, 
+                       lambda = lambda))
+  }
   
   if (any(sapply(result, function(res) inherits(res, "try-error")))) {
     if (trace) cat("Nelder-Mead used in CM 2 \n")
     result <- lapply(1:K,
-                     function(j) parallel::mcparallel(expr(j, "Nelder-Mead"), name = j))
+                     function(j) parallel::mcparallel(try(cm.step.2.component(j = j, 
+                                                                              method = "Nelder-Mead", 
+                                                                              x = x, 
+                                                                              margins = margins,
+                                                                              component = mvdc[[j]], 
+                                                                              z = z, 
+                                                                              lambda = lambda))
+                                                      , name = j))
     result <- parallel::mccollect(result)
     
     if (any(sapply(result, function(res) inherits(res, "try-error")))) {
@@ -129,4 +78,38 @@ cm.step.2 <- function(x, K, z, mvdc, margins, lambda, trace = T) {
   
   return(list(mvdc = mvdc,
               penalty = total_penalty))
+}
+
+
+
+
+cm.step.2.component <- function(j, method, x, margins, component, z, lambda) {
+  # compute cumulative probabilities for given mixture component
+  u <- compute.u(x = x, margins = margins, marginal_params = component@paramMargins)
+  u[u > 0.999] <- 0.999
+  u[u < 0.001] <- 0.001
+  
+  # prepare copula parameters for optimization 
+  start <- component@copula@parameters
+  start <- P2p.angles(rho2angles(copula::p2P(start)))
+  start <- trans.ang(start)
+  
+  # use optimizer to find likelihood maximum
+  optim_out <- stats::optim(par = start, 
+                            fn = CM.2.optimiser,
+                            method = method,
+                            control = list(fnscale = -1, trace = F),
+                            post_probs = z[, j], 
+                            u = u, 
+                            copula = component@copula,
+                            lambda = lambda)
+  
+  # collate and return shrinkage penalty and new value of copula parameters returned by optimizer
+  res <- optim_out$par
+  res <- inversetrans.ang(res)
+  penalty <- lambda * sum((res - (pi/2))^2)
+  
+  res <- list(param = copula::P2p(angles2rho(p2P.angles(res))), 
+              pen = penalty)
+  return(res)
 }
